cbea1ce476113971b07f8c76923e6663c0138561
Austin Flores <ctrl4ltdeleteme@gmail.com>
2016-11-04 13:46:37 +0100

    Removing old style comments

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,3 @@
-=begin
-create a program that reads in a csv
-
-Goal:
-Given a set of people,
-With differing availability,
-Create a schedule such that:
-  No person volunteers twice a day.
-  No person volunteers at the the end of a day and then the beginning of a day
-
-=end
-  
 class Person
 
   attr_accessor :first,:last,:availability
